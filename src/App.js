0c673053908277bb1f6396f9c6e0faa3efa932b8
Thuynguyen729 <117136080+Thuynguyen@users.noreply.github.com>
2023-03-14 22:16:54 +0700

    add config-overide-cra

--- src/App.js
+++ src/App.js
@@ -1,31 +1,5 @@
-import { Routes, Route, Link } from "react-router-dom"
-import Home from "./Page/Home"
-import Content from "./Page/Content"
-import Contact from "./Page/Contact"
-
 function App() {
-  return (
-    <div>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/content">Content</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact</Link>
-          </li>
-        </ul>
-      </nav>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/content" element={<Content />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </div>
-  )
+  return <div>hello</div>
 }
 
 export default App
