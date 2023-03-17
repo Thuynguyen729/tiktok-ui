5f369775faa4357c2e24af1548806eb6dccbb0ca
Thuynguyen729 <117136080+Thuynguyen@users.noreply.github.com>
2023-03-17 16:38:44 +0700

    build router and base layout

---
+++ src/globalComponents/Layouts/index.js
@@ -0,0 +1,7 @@
+// import DefaultLayout from './DefaultLayout';
+// export default DefaultLayout
+// equal to :
+
+export { default as DefaultLayout } from './DefaultLayout';
+//
+export { default as HeaderOnly } from './HeaderOnly';
